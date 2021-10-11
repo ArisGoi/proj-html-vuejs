dcae30ac96c09a508bc9c2b81013fecfc978dd07
ArisGoi <aris.goi97@gmail.com>
2021-10-11 14:26:06 +0200

    struttura iniziale

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/style.scss';
+
+</style>
