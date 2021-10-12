b7db977c40924e9495375adf2ae64a7830d9215c
ArisGoi <aris.goi97@gmail.com>
2021-10-12 15:33:32 +0200

    add farm to table left

--- src/components/index-comp/farmToTable.vue
+++ src/components/index-comp/farmToTable.vue
@@ -5,7 +5,7 @@
         <div class="headline">
             <h2>Farm to Table</h2>
             <h5>Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non nunc lacinia lobortis.</h5>
-            
+
             <button class="btn-1">
                 Read our blog
                 <img src="../../assets/icons/blog.svg" alt="">
@@ -13,7 +13,35 @@
         </div>
 
         <div class="content">
-            
+            <div class="side-left">
+
+                <div class="big-article">
+                    <div class="img">
+                      <img src="../../assets/images/Yogurt-Nan.jpg" alt="">
+                  </div>
+                  <div class="body">
+                      <h3>Food Corner: Top Japanese Restaurant for Sushi</h3>
+                      <h5>By admin | March 25th, 2019</h5>
+                      <div class="hr"></div>
+                      <p>Suspendisse at semper odio. Nam fringilla scelerisque tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec tincidunt posuere ornare. Phasellus placerat odio non feugiat sollicitudin. Integer vita elementum ex.</p>
+                  </div>
+                  <div class="more-bar">
+                      <a href="">Read More ></a>
+                      <img src="../../assets/icons/chat.svg" alt="">
+                      <span>0</span>
+                  </div>
+                </div>
+
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+                <cardSmall img="single-post-img3.jpg" title="Food Corner: Top Japanese Restaurants for Sushi" authorDate="By admin | March 25th, 2019"/>
+
+                <button class="btn-2">Load More Posts</button>
+            </div>
+            <div class="side-right"></div>
         </div>
 
       </div>
@@ -21,9 +49,12 @@
 </template>
 
 <script>
+import cardSmall from './cardSmall.vue'
+
 export default {
   name: 'farmToTable',
   components:{
+      cardSmall,
   },
   // props: {
   //   msg: String
@@ -58,5 +89,80 @@ export default {
     }
 }
 
+.content{
+    display: flex;
+    justify-content: space-between;
+}
+
+.side-left{
+    width: calc(65% - 10px);
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    div{
+        margin-top: 30px;
+        &:first-child{
+            margin-top: 0px;
+        }
+    }
+
+    .big-article{
+        width: 100%;
+        background-color: $white;
+
+        .img{
+            width: 100%;
+            height: 500px;
+            img{ @include fit-cover; }
+        }
+
+        .body{
+            padding: 40px 30px;
+            .hr{
+                height: 1px;
+                width: 100%;
+                background-color: $c-bw-1;
+                margin: 20px 0;
+            }
+            h3{
+                @include title5;
+                margin-bottom: 8px;
+            }
+            h5, p{ @include corpo; }
+            button{
+                cursor: pointer;
+                margin-top: 25px;
+            }
+        }
+
+        .more-bar{
+            text-align: right;
+            padding: 0 30px 30px 30px;
+            a{
+                text-decoration: none;
+                color: $text-black;
+                float: left;
+                @include corpo;
+            }
+            img{ height: 18px; }
+            span{
+                @include corpo;
+                margin-left: 5px;
+            }
+        }
+    }
+
+    .btn-2{
+        width: 100%;
+        margin-top: 30px;
+    }
+}
+
+.side-right{
+    width: calc(35% - 20px);
+    height: 300px;
+    border: 1px solid royalblue;
+}
 
 </style>
