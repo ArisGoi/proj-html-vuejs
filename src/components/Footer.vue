95aa4a07cd311204c1f9c09fd9eb18506da0e115
ArisGoi <aris.goi97@gmail.com>
2021-10-13 15:25:23 +0200

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,6 +6,23 @@
       <div class="container">
         <div class="block">
           <img class="logo" src="../assets/images/avada-food-logo-mob-2x.png" alt="avada food logo">
+          <ul class="social">
+            <li><a href="https://www.facebook.com/" target="_blank">
+              <img src="../assets/icons/facebook.svg" alt="Facebook logo">
+            </a></li>
+            <li><a href="https://www.instagram.com/" target="_blank">
+              <img src="../assets/icons/instagram.svg" alt="Instagram logo">
+            </a></li>
+            <li><a href="https://www.twitter.com/" target="_blank">
+              <img src="../assets/icons/twitter.svg" alt="Twitter logo">
+            </a></li>
+            <li><a href="https://www.youtube.com/" target="_blank">
+              <img src="../assets/icons/youtube.svg" alt="YouTube logo">
+            </a></li>
+            <li><a href="https://www.pinterest.com/" target="_blank">
+              <img src="../assets/icons/pinterest.svg" alt="Pinterest logo">
+            </a></li>
+          </ul>
         </div>
         <div class="block">
           <ul>
@@ -18,16 +35,25 @@
         </div>
         <div class="block">
           <h3>Contact</h3>
-          <ul>
-            <li>775 New York Ave, Brooklyn, Kings, New York 11203</li>
-            <li>+0100-505-0000</li>
-            <li>info@your-domain.com</li>
+          <ul class="businessC">
+            <li>
+              <img src="../assets/icons/location.svg" alt="address icon">
+              775 New York Ave, Brooklyn, Kings, New York 11203
+            </li>
+            <li>
+              <img src="../assets/icons/phone.svg" alt="phone icon">
+              +0100-505-0000
+            </li>
+            <li>
+              <img src="../assets/icons/mail.svg" alt="mail icon">
+              info@your-domain.com
+            </li>
           </ul>
         </div>
-        <div class="block">
+        <div class="block" id="dwnApp">
           <h3>Get our App!</h3>
-          <img src="" alt="Click to download our app from GooglePlay">
-          <img src="" alt="Click to download our app from AppStore">
+          <img src="../assets/images/download-app-store.png" alt="Click to download our app from GooglePlay">
+          <img src="../assets/images/download-app-store.png" alt="Click to download our app from AppStore">
         </div>
       </div>
     </div>
@@ -66,7 +92,7 @@ footer{
 
   .block{
     width: calc(100% / 4);
-    padding: 15px;
+    padding: 15px 25px;
     border-right: 1px solid $c-bw-1;
 
     &:last-child{
@@ -74,9 +100,56 @@ footer{
     }
   }
 
-  img.logo{
-      width: 100%;
+  ul{
+    list-style: none;
+    li{
+      margin-top: 15px;
+      color: $text-gray;
+      @include corpo;
+      font-size: .8125rem;
+    }
+    a{
+      color: $text-black;
+      text-decoration: none;
+      font-size: 1rem;
+    }
   }
+  ul.businessC{
+    li{
+      display: flex;
+      align-items: center;
+    }
+    img{
+      width: .9375rem;
+      margin-right: 10px;
+    }
+  }
+  ul.social{
+    margin-top: 15px;
+    li{
+      display: inline-block;
+      margin-right: 10px;
+    }
+    img{
+      width: 1.2rem;
+      margin-right: 10px;
+    }
+  }
+
+  h3{
+    @include title4;
+    color: $c-main;
+    font-size: 1.125rem;
+    padding-bottom: 10px;
+  }
+}
+img.logo{
+  width: 100%;
+}
+#dwnApp img{
+  width: 70%;
+  margin-top: 10px;
+  cursor: pointer;
 }
 
 
